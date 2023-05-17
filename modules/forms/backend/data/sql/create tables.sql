ca2d903c7846524986df4eb199af74cba7c3ab49
Paweł Benetkiewicz <git@benio.me>
2023-05-17 22:12:25 +0200

    feat: add sql variables for forms module

--- modules/forms/backend/data/sql/create tables.sql
+++ modules/forms/backend/data/sql/create tables.sql
@@ -1,3 +1,8 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
 CREATE TABLE IF NOT EXISTS `forms` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user` int(10) unsigned NOT NULL,
--- modules/forms/backend/data/sql/drop tables.sql
+++ modules/forms/backend/data/sql/drop tables.sql
@@ -1,1 +1,6 @@
-DROP TABLE IF EXISTS `forms`;
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS forms;
