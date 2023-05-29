a396b53e95f732019fe9d5c2d5aa2d725a755da4
Mikołaj Trawiński <kontakt@mikolajtrawinski.pl>
2023-05-29 18:17:09 +0200

    refactoring, added phpmyadmin

--- db-init-scripts/init-config.sql
+++ db-init-scripts/init-config.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS bacon CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER 'bacon'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON bacon.* TO 'bacon'@'%';
+GRANT ALL ON bacon.* TO 'bacon'@'%';
 SET GLOBAL event_scheduler = ON;
